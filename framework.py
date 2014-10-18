b2ee94ac5b47dec72a887fccd3e5911bd04c860f
rvanderheyde <radmer.vanderheyde@students.olin.edu>
2014-10-18 13:13:56 -0400

    skeleton of what we need

---
+++ framework.py
@@ -0,0 +1,31 @@
+def done(brd):
+	#Return True if game is won or if game is unwinnable. Print some statement based on the condition.
+	return True
+
+def otherPlayer(player):
+	#switch between players 
+	return True
+
+def validateMove(brd, move):
+	#detects if a move is legal
+	return True
+
+def move(brd):
+	#Makes a legal move based on current game rules
+	return True
+
+def getState():
+	#detects how the board looks
+	return True
+
+def createBoard():
+	return True
+
+def run():
+	return True
+
+def main():
+	return True
+
+if __name__ in "__main__":
+	main()
