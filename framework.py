087e09489743f3ed9ee014385ff8f68e92405dc8
Filippos Lymperopoulos <filippos.lymperopoulos@students.olin.edu>
2014-10-18 15:45:26 -0400

    Pawn validating works

--- framework.py
+++ framework.py
@@ -1,27 +1,31 @@
+import chess
+
 def done(brd):
-	#Return True if game is won or if game is unwinnable. Print some statement based on the condition.
+	"""Return True if game is won or if game is unwinnable. Print some statement based on the condition."""
 	return True
 
 def otherPlayer(player):
-	#switch between players 
+	"""switch between players""" 
 	return True
 
 def validateMove(brd, move):
-	#detects if a move is legal
+	"""detects if a move is legal"""
 	return True
 
 def move(brd):
-	#Makes a legal move based on current game rules
+	"""Makes a legal move based on current game rules"""
 	return True
 
 def getState():
-	#detects how the board looks
+	"""detects how the board looks"""
 	return True
 
 def createBoard():
+        """ Creates board"""
 	return True
 
 def run():
+        """ Run method for the file"""
 	return True
 
 def main():
