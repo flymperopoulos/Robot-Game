451b4f1dc3252e94efce039e47be6333cf96d6c7
rvanderheyde <radmer.vanderheyde@students.olin.edu>
2014-10-21 11:30:04 -0400

    Framework done, though there may be errors

--- framework.py
+++ framework.py
@@ -7,20 +7,28 @@ def printBoardttt(brd):
 	brdList = []
 	for i in range(1,4):
 		for j in range(1,4):
-			if board[(i,j)] == 0:
+			if brd[(i,j)] == 0:
 				brdList.append(' ')
 			else:
-				brdList.append(board[(i,j)].name)
+				brdList.append(brd[(i,j)].name)
 
 def printBoard(brd):
 	"""displays board"""
 	brdList = []
 	for i in range(1,9):
 		for j in range(1,9):
-			if board[(i,j)] == 0:
+			if brd[(i,j)] == 0:
 				brdList.append(' ')
 			else:
-				brdList.append(board[(i,j)].name)
+				brdList.append(brd[(i,j)].name)
+	print brd[0:8] 
+	print brd[9:16]
+	print brd[17:24]
+	print brd[25:32]
+	print brd[33:40]
+	print brd[41:48]
+	print brd[49:56]
+	print brd[57:64]
 
 def done(game, brd):
 	"""Return True if game is won or if game is unwinnable. Print some statement based on the condition."""
@@ -47,11 +55,7 @@ def otherPlayer(player):
 
 def validateMove(game, brd, move):
 	"""detects if a move is legal"""
-	if move in game.validateMoves():
-		return True
-	else: 
-		print "Not a valid move"
-		return False
+	return True
 
 def readPlayerInput(brd):
 	while True:
@@ -60,15 +64,13 @@ def readPlayerInput(brd):
 			sys.exit(0)
 		lis = usrInput.split()
 		if len(move) == 5:
-			for i in board
-				if board[i] != 0:
-					if (lis[1],lis[2]) in board[i].possibleMoves and board[i].name == lis[0]:
+			for i in brd:
+				if brd[i] != 0:
+					if (lis[1],lis[2]) in brd[i].possibleMoves() and brd[i].name == lis[0]:
 						move[0] = board[i]
 						move[1] = int(move[1])
 						move[2] = int(move[2])
 						return move
-				else: 
-					print "not a piece "
 		else:
 			print "Invalid length. Try again."
 		
@@ -96,32 +98,35 @@ def getState():
 def createBoard(game):
 	"""initializes board"""
 	if game.lowercase() == "chess":
-		return chess.initalboard()
+		game = chess
+		return game, chess.initalboard()
 	elif game.lowercase() == "checkers":
-		return checkers.initalboard()
+		game = checkers
+		return game, checkers.initalboard()
 	else:
-		return tictactoe.initalboard()
+		game = tictactoe
+		return game, tictactoe.initalboard()
 	return True
 
 def run(game,curPlayer,playW, playB):
-	brd = createBoard(game)
+	game, brd = createBoard(game)
 
 	printBoard(brd)
 	
 	player = curPlayer
 	while not done(brd):
 		if player = 'W':
-			move = playW(brd)
+			move = playW(brd, player)
 		else:
-			move = playB(brd)
-		makeMove(game,brd,move)
+			move = playB(brd, player)
+		brd = makeMove(game,brd,move)
 		printBoard(brd)
 		player = otherPlayer()
 	if hasWin(brd):
 		print "winner " + player
 	else: 
 		print "Stalemate"
-		
+
 def main():
 	game, brd = getState()
 	inpt = raw_input("Game, Current player(w or b), Human or Computer, Human or Computer.")
