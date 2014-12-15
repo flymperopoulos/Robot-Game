b4adf1882a087c8a32598069682e255bdeeede1b
acejang1994 <byumho.jang@students.olin.edu>
2014-12-14 21:54:37 -0500

    preparing to merge

--- boardRecognition.py
+++ boardRecognition.py
@@ -20,24 +20,28 @@ import numpy as np
 # img = cv2.imread('circle.jpg',0)
 # edges = cv2.Canny(img,100,200)
 
-im = cv2.imread('res/boardStateOne.jpg')
-# im = cv2.imread('res/vidMovedCheckers.png')
+# im = cv2.imread('res/boardStateTwo.jpg')
+# im = cv2.imread('res/boardStateOne.jpg') 
+im = cv2.imread('res/boardBlue.jpg')
+im = cv2.imread('res/red.jpg')
+# print im
 
 height, width = im.shape[:2]
+print height, width
 imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
 hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
 
 # hsv bounds for red blue and green
-lower_red = np.uint8([0, 155, 0])
+ 
 lower_red = np.uint8([0, 155, 0])
 upper_red = np.uint8([10, 255, 255])
-upper_red = np.uint8([20, 205, 225])
-lower_blue = np.uint8([100,150,0])
-upper_blue = np.uint8([140,255,255])
+
+lower_blue = np.uint8([100, 50, 50])
+upper_blue = np.uint8([140, 255, 255])
 lower_green = np.uint8([40, 110, 110])
 upper_green = np.uint8([70, 255, 255])
 
-mask = cv2.inRange(hsv, lower_red, upper_red)
+mask = cv2.inRange(hsv, lower_blue, upper_blue)
 output = cv2.bitwise_and(im, im, mask = mask)
 # print len(np.where(mask != 0)[0])
 # print len(np.where(mask != 0)[0])
@@ -59,11 +63,12 @@ for cnt in contours:
 		cv2.circle(mask,(x,y),3,(0,255,0),2)
 # print i
 color = list(set(color))
+print color
 
 def checkColorArea():
 	pass
 
-cv2.imshow("im", im)
+# cv2.imshow("im", im)
 cv2.imshow("red", mask)
 
 """
@@ -84,7 +89,7 @@ for cnt in contours:
 	m = cv2.moments(cnt)
 
 	# print cnt, len(cnt)
-	print m['m00']
+	# print m['m00']
 	if m['m00'] > big:
 		big = m['m00']
 		c = cnt
@@ -105,6 +110,11 @@ for cnt in contours:
 		pieces.append((x,y))
 		cv2.circle(imgray,(x,y),3,(0,255,0),5)
 
+	if m['m00'] < 1300 and m['m00'] > 1000:
+		x,y = int(m['m10']/m['m00']), int(m['m01']/m['m00'])
+		pieces.append((x,y))
+		cv2.circle(imgray,(x,y),3,(0,255,0),5)
+
 pieces = list(set(pieces))
 
 # print big, c, mom
@@ -115,31 +125,32 @@ cv2.imshow('stuff', imgray)
 cv2.imshow('img', img)
 
 
-gridW = -(centerX - cornerX)*2/8
-gridH = (centerY - cornerY)*2/8
-print gridW, gridH
-print centerX, centerY
-print cornerX, cornerY
-print pieces
 
-d = {}
-xtemp=0
-ytemp =0 
-for p in pieces:
-	xtemp = -(p[0]-cornerX)/gridW +1
-	ytemp = (p[1]-cornerY)/gridH +1
-	if xtemp < 9 and ytemp < 9  and xtemp >0  and ytemp > 0:
-		d[xtemp, ytemp] = 1
-print d, len(d)
+# gridW = -(centerX - cornerX)*2/8
+# gridH = (centerY - cornerY)*2/8
+# print gridW, gridH
+# print centerX, centerY
+# print cornerX, cornerY
+# print pieces
 
-dc = {}
-xtemp=0
-ytemp =0 
-for p in color:
-	xtemp = -(p[0]-cornerX)/gridW +1
-	ytemp = (p[1]-cornerY)/gridH +1
-	if (xtemp, ytemp) in d:
-		dc[xtemp, ytemp] = "R"
-print dc, len(dc)
+# d = {}
+# xtemp=0
+# ytemp =0 
+# for p in pieces:
+# 	xtemp = -(p[0]-cornerX)/gridW +1
+# 	ytemp = (p[1]-cornerY)/gridH +1
+# 	if xtemp < 9 and ytemp < 9  and xtemp >0  and ytemp > 0:
+# 		d[xtemp, ytemp] = 1
+# print d, len(d)
+
+# dc = {}
+# xtemp=0
+# ytemp =0 
+# for p in color:
+# 	xtemp = -(p[0]-cornerX)/gridW +1
+# 	ytemp = (p[1]-cornerY)/gridH +1
+# 	if (xtemp, ytemp) in d:
+# 		dc[xtemp, ytemp] = "R"
+# print dc, len(dc)
 
 cv2.waitKey(0)
